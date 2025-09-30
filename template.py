f50ac26b67b7ce9cb21d0d0f6fabc1481f1c9e35
YoussefBedeer <yousser1bedeer@gmail.com>
2025-09-30 14:12:59 +0300

    Create project template

---
+++ template.py
@@ -0,0 +1,43 @@
+import os 
+import sys 
+import logging 
+from pathlib import Path
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s')
+
+project_name = "cnnClassifier"
+
+list_of_files = [
+    ".github/workflows/.gitkeep",
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/config/__init__.py",
+    f"src/{project_name}/config/configuration.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/constants/__init__.py",
+    "config/config.yaml",
+    "dvc.yaml",
+    "params.yaml",
+    "requirements.txt",
+    "setup.py",
+    "research/trials.ipynb",
+    "templates/index.html"
+]
+
+for file_path in list_of_files:
+    file_path = Path(file_path)
+    dir, file = os.path.split(file_path)
+
+    if dir != "":
+        os.makedirs(dir, exist_ok=True)
+        logging.info(f"created {dir}")
+
+    if (not os.path.exists(file_path)) or (os.path.getsize(file_path) == 0):
+        with open(file_path, "w") as f:
+            pass
+        logging.info(f"{file_path} created")
+
+    else:
+        logging.info(f"{file_path} exists")
+
